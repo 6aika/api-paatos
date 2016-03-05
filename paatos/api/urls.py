1a067a02e4679d762f7f0e4a6a72c4946d9072aa
Aapo Rista <aapo.gitpub@rista.net>
2016-03-05 15:36:01 +0200

    Switch to use ViewSets

--- paatos/api/urls.py
+++ paatos/api/urls.py
@@ -1,10 +1,17 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
+# from django.conf.urls import url
+# from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-urlpatterns = [
-    url(r'^case/$', views.CaseList.as_view()),
-    # url(r'^snippets/(?P<pk>[0-9]+)/$', views.CaseDetail.as_view()),
-]
+from rest_framework import routers
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+router = routers.DefaultRouter()
+router.register(r'case', views.CaseViewSet)
+urlpatterns = router.urls
+
+# urlpatterns = [
+    # url(r'^case/$', views.CaseList.as_view()),
+    # url(r'^snippets/(?P<pk>[0-9]+)/$', views.CaseDetail.as_view()),
+# ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns += router.urls
--- paatos/api/views.py
+++ paatos/api/views.py
@@ -3,17 +3,24 @@ from django.shortcuts import render
 from decisions.models import Case
 from api.serializers import CaseSerializer
 from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+# from rest_framework.views import APIView
+# from rest_framework import exceptions, filters, mixins, serializers, viewsets
+from rest_framework import viewsets
+
+# from rest_framework.response import Response
+# from rest_framework import status
 
 
-class CaseList(APIView):
+# class CaseList(APIView):
+class CaseViewSet(viewsets.ReadOnlyModelViewSet):
     """
     List all cases
     """
+    queryset = Case.objects.all()
+    serializer_class = CaseSerializer
 
-    def get(self, request, format=None):
-        case = Case.objects.all()
-        serializer = CaseSerializer(case, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return self.queryset
+        else:
+            return self.queryset.filter(public=True)
