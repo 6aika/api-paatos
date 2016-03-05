3c7582d302e415842f11bac5c06af36f462d6560
Ville Koivunen <ville.koivunen@forumvirium.fi>
2016-03-05 19:47:56 +0200

    verbose_name -> help_text

--- paatos/decisions/models.py
+++ paatos/decisions/models.py
@@ -6,21 +6,22 @@ from django.utils.translation import ugettext_lazy as _
 
 class Case(models.Model):
     iri = models.CharField(max_length=255,
-                           verbose_name=_('IRI for this case'))
+                           help_text=_('IRI for this case'))
     title = models.CharField(max_length=255,
-                             verbose_name=_('A high level matter to be decided'))
+                             help_text=_('A high level matter to be decided'))
     description = models.CharField(max_length=255, blank=True,
-                                   verbose_name=_('A descriptive compact title for the case'))
+                                   help_text=_('A descriptive compact title for the case'))
     summary = models.CharField(max_length=255, blank=True,
-                               verbose_name=_('Summary of this case. Typically a few sentences.'))
+                               help_text=_('Summary of this case. Typically a few sentences.'))
     attachments = models.ManyToManyField('Attachment')
     category = models.CharField(max_length=255, blank=True,
-                                verbose_name=_('Category this case belongs to ("tehtäväluokka")'))
+                                help_text=_('Category this case belongs to ("tehtäväluokka")'))
     area = models.ForeignKey('Area', null=True,
-                             verbose_name=_('Geographic area this case is related to'))
+                             help_text=_('Geographic area this case is related to'))
     originator = models.ForeignKey('Person', null=True)
     creation_date = models.DateField(blank=True, null=True)
     public = models.BooleanField(default=True)
+    related_case = models.ManyToManyField('self', help_text=_('Other related cases'))
 
     def __str__(self):
         return self.title
@@ -28,7 +29,7 @@ class Case(models.Model):
 
 class Post(models.Model):
     label = models.CharField(max_length=255,
-                             verbose_name=_("Post's label"))
+                             help_text=_("Post's label"))
     organization = models.ForeignKey('Organization')
 
     def __str__(self):
@@ -36,7 +37,7 @@ class Post(models.Model):
 
 
 class Organization(models.Model):
-    abstract = models.CharField(max_length=255, verbose_name=_('A one-line description of an organization'))
+    abstract = models.CharField(max_length=255, help_text=_('A one-line description of an organization'))
 
     def __str__(self):
         return self.abstract
@@ -44,21 +45,23 @@ class Organization(models.Model):
 
 class Action(models.Model):
     iri = models.CharField(max_length=255,
-                           verbose_name=_('IRI for this action'))
+                           help_text=_('IRI for this action'))
     title = models.CharField(max_length=255,
-                             verbose_name=_('Descriptive compact title for this case'))
-    case = models.ForeignKey(Case, verbose_name=_('Case this action affects'))
-    ordering = models.IntegerField(verbose_name=_('Ordering of this action within a meeting'))
-    article_number = models.CharField(max_length=255,
-                                      verbose_name=_('The article number given to this action after decision'))
+                             help_text=_('Descriptive compact title for this case'))
+    case = models.ForeignKey(Case, help_text=_('Case this action affects'))
+    ordering = models.IntegerField(help_text=_('Ordering of this action within a meeting'))
+    article_number = models.CharField(max_length=255, null=True,
+                                      help_text=_('The article number given to this action after decision'))
     proposal_identifier = models.CharField(max_length=255,
-                                           verbose_name=_(
+                                           help_text=_(
                                                'Identifier for this action used inside the meeting minutes. The format will vary between cities.'))
-    responsible_party = models.ForeignKey(Organization, verbose_name=_(
+    resolution = models.CharField(max_length=255, blank=True, null=True,
+                                  help_text="Resolution taken in this action (like tabled, decided...)")
+    responsible_party = models.ForeignKey('Organization', help_text=_(
         'The city organization responsible for this decision. If decision is delegated, this is the organization that delegated the authority.'))
-    delegation = models.ForeignKey(Post, null=True, verbose_name=_(
+    delegation = models.ForeignKey('Post', blank=True, null=True, help_text=_(
         'If this decision was delegated, this field will be filled and refers to the post that made the decision'))
-    event = models.ForeignKey('Event', verbose_name=_('Event (if any) where this action took place'), null=True)
+    event = models.ForeignKey('Event', help_text=_('Event (if any) where this action took place'), null=True)
     # Contents for this action refer to this
     # Votes for this action refer here
 
@@ -67,22 +70,22 @@ class Action(models.Model):
 
 
 class Content(models.Model):
-    iri = models.CharField(max_length=255, verbose_name=_('IRI for this content'))
-    ordering = models.IntegerField(verbose_name=_('Ordering of this content within the larger context (like action)'))
-    title = models.CharField(max_length=255, verbose_name=_('Title of this content'))
-    type = models.CharField(max_length=255, verbose_name=_(
+    iri = models.CharField(max_length=255, help_text=_('IRI for this content'))
+    ordering = models.IntegerField(help_text=_('Ordering of this content within the larger context (like action)'))
+    title = models.CharField(max_length=255, help_text=_('Title of this content'))
+    type = models.CharField(max_length=255, help_text=_(
         'Type of this content (options include: decision, proposal, proceedings...)'))
-    hypertext = models.CharField(max_length=255, verbose_name=_(
+    hypertext = models.CharField(max_length=255, help_text=_(
         'Content formatted with pseudo-HTML. Only a very restricted set of tags is allowed. These are: first and second level headings (P+H1+H2) and table (more may be added, but start from a minimal set)'))
-    action = models.ForeignKey('Action', verbose_name=_('Action that this content describes'))
+    action = models.ForeignKey('Action', help_text=_('Action that this content describes'))
 
     def __str__(self):
         return self.title
 
 
 class Attachment(models.Model):
-    iri = models.CharField(max_length=255, verbose_name=_('IRI for this attachment'))
-    file = models.CharField(max_length=255, verbose_name=_('FIXME: i should refer to a file'))
+    iri = models.CharField(max_length=255, help_text=_('IRI for this attachment'))
+    file = models.CharField(max_length=255, help_text=_('FIXME: i should refer to a file'))
     # content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     # object_id = models.PositiveIntegerField()
     # content_object = GenericForeignKey('content_type', 'object_id')
@@ -92,9 +95,9 @@ class Attachment(models.Model):
 
 # "An event is an occurrence that people may attend."
 class Event(models.Model):
-    name = models.CharField(max_length=255, verbose_name=_("The event's name"))
-    organization = models.ForeignKey(Organization, verbose_name=_('The organization organizing the event'))
-    attendees = models.ManyToManyField('Person', verbose_name=_('People attending this event'), through='Attendance')
+    name = models.CharField(max_length=255, help_text=_("The event's name"))
+    organization = models.ForeignKey(Organization, help_text=_('The organization organizing the event'))
+    attendees = models.ManyToManyField('Person', help_text=_('People attending this event'), through='Attendance')
 
     def __str__(self):
         return self.name
@@ -103,11 +106,11 @@ class Event(models.Model):
 class Attendance(models.Model):
     event = models.ForeignKey('Event')
     attendee = models.ForeignKey('Person')
-    role = models.CharField(max_length=50, verbose_name=_('Role of the person in the event (chairman, secretary...'))
+    role = models.CharField(max_length=50, help_text=_('Role of the person in the event (chairman, secretary...'))
 
 
 class VoteEvent(models.Model):
-    legislative_session = models.ForeignKey('Event', verbose_name=_('The meeting (event) where this vote took place'))
+    legislative_session = models.ForeignKey('Event', help_text=_('The meeting (event) where this vote took place'))
     vote_count = models.ForeignKey('VoteCount')
 
 
@@ -116,21 +119,21 @@ class VoteCount(models.Model):
 
 
 class Membership(models.Model):
-    person = models.ForeignKey('Person', verbose_name=_('Person who has membership in organization'))
-    post = models.ForeignKey('Post', verbose_name=_('The post held by the member through this membership'))
+    person = models.ForeignKey('Person', help_text=_('Person who has membership in organization'))
+    post = models.ForeignKey('Post', help_text=_('The post held by the member through this membership'))
     organization = models.ForeignKey('Organization',
-                                     verbose_name=_('The organization in which the person or organization is a member'))
+                                     help_text=_('The organization in which the person or organization is a member'))
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=255, verbose_name=_("A person's preferred full name"))
+    name = models.CharField(max_length=255, help_text=_("A person's preferred full name"))
 
     def __str__(self):
         return self.name
 
 
 class Area(models.Model):
-    name = models.CharField(max_length=255, verbose_name=_("Area's name"))
+    name = models.CharField(max_length=255, help_text=_("Area's name"))
 
     def __str__(self):
         return self.name
