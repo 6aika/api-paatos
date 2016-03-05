bad752642d3bbca7975047799476f5f537ae0a09
Aapo Rista <aapo.gitpub@rista.net>
2016-03-05 16:04:16 +0200

    Add more models to admin

--- paatos/decisions/admin.py
+++ paatos/decisions/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from decisions.models import Case, Action
+from decisions.models import Case, Action, Organization, Post, Event
 
 
 class CaseAdmin(admin.ModelAdmin):
     fields = ('iri', 'title', 'description', 'summary', 'category')
+
 admin.site.register(Case, CaseAdmin)
 admin.site.register(Action)
+admin.site.register(Organization)
+admin.site.register(Post)
+admin.site.register(Event)
