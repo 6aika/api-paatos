d2d08abc55ed2d0346908f6fe2ce04e9d6e8c34e
Aapo Rista <aapo.gitpub@rista.net>
2016-03-05 14:53:30 +0200

    Add intial Case serializer

---
+++ paatos/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from decisions.models import Case
+
+
+class CaseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Case
+        fields = ('id', 'iri', 'title', )
