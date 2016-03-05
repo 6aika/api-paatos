14ce152aa4643879c66f825a9ca3a07c292adf7d
Aapo Rista <aapo.gitpub@rista.net>
2016-03-05 14:54:31 +0200

    Add CaseList view

--- paatos/api/views.py
+++ paatos/api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from decisions.models import Case
+from api.serializers import CaseSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class CaseList(APIView):
+    """
+    List all cases
+    """
+
+    def get(self, request, format=None):
+        case = Case.objects.all()
+        serializer = CaseSerializer(case, many=True)
+        return Response(serializer.data)
