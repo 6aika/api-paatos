11e869e3dc2c89ad7daa10864d0de67bee77098e
Aapo Rista <aapo.gitpub@rista.net>
2016-03-05 15:49:05 +0200

    Add Case admin

--- paatos/decisions/admin.py
+++ paatos/decisions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from decisions.models import Case
 
-# Register your models here.
+
+class CaseAdmin(admin.ModelAdmin):
+    fields = ('iri', 'title', 'description', 'summary', 'category')
+admin.site.register(Case, CaseAdmin)
+
