9c1130c105571d4fecc69134f81500903893a2a2
Ville Koivunen <ville.koivunen@forumvirium.fi>
2016-03-05 15:50:44 +0200

    Register action with admin

--- paatos/decisions/admin.py
+++ paatos/decisions/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from decisions.models import Case
+from decisions.models import Case, Action
 
 
 class CaseAdmin(admin.ModelAdmin):
     fields = ('iri', 'title', 'description', 'summary', 'category')
 admin.site.register(Case, CaseAdmin)
-
+admin.site.register(Action)
